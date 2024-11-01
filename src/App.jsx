d027abed2008b84eb344c95c3c73fc2571c53fae
garcia-lopez <mplg070605@gmail.com>
2024-11-01 12:51:09 -0600

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Logotipo_square from './components/Logotipo_square';
 function App() {
   return (
     <>
-      <div className='bg-TD-LightPink w-screen min-h-screen m-0 pt-6 pb-6 pl-4 pr-6'>
+      <div className='bg-TD-LightPink w-screen min-h-screen m-0 pt-6 pb-6 pl-9 pr-9'>
       <Logotipo_square></Logotipo_square>
       </div>
     </>
--- src/components/Logotipo_square.jsx
+++ src/components/Logotipo_square.jsx
@@ -3,20 +3,14 @@ import '../../src/App.css';
 function Logotipo_square() {
     return (
         <>
-        <div className='bg-white w-full h-[200px] rounded-[80px] sm:w-[500px] sm:h-[250px] sm:p-9  sm:rounded-[80px]'>
-           <div className='grid justify-self-center'>
-            <p 
-              className='font_logotipo'>
-                The tote
-            </p>
-            <p 
-              className='font_dream'>
-                DREAM
-            </p>
+        <div className='bg-white w-full min-h-[200px] rounded-[80px] overflow-hidden sm:w-[500px] sm:h-[250px] sm:pt-9 sm:rounded-[80px] container'>
+          <div className='grid justify-items-start sm:justify-items-start'>
+            <p className='font_logotipo'>The tote</p>
+            <p className='font_dream'>DREAM</p>
+          </div>
         </div>
-        </div>
-
-    </>
+      </>
+      
 
     )
 }
