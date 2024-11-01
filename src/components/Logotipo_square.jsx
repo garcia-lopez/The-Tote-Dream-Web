442aa046aca276ba820ff31598c7e4954e983a3d
garcia-lopez <mplg070605@gmail.com>
2024-11-01 13:29:24 -0600

    feat: added a leaf

--- src/components/Logotipo_square.jsx
+++ src/components/Logotipo_square.jsx
@@ -1,4 +1,5 @@
 import '../../src/App.css';
+import leafVector from '../../src/assets/leaf_vector.svg';
 
 function Logotipo_square() {
     return (
@@ -6,6 +7,7 @@ function Logotipo_square() {
         <div className='bg-white w-full min-h-[200px] rounded-[80px] overflow-hidden sm:w-[500px] sm:h-[250px] sm:pt-9 sm:rounded-[80px] container'>
           <div className='grid justify-items-start sm:justify-items-start'>
             <p className='font_logotipo'>The tote</p>
+            <img src={leafVector} alt="Description of image" className='leaf' />
             <p className='font_dream'>DREAM</p>
           </div>
         </div>
