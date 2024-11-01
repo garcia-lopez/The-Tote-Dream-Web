882e5361b989d05b32250791402d41f9d47a8d05
garcia-lopez <mplg070605@gmail.com>
2024-11-01 11:45:15 -0600

    feat: Added new font, I also try to do it responsive

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import Logotipo_square from './components/Logotipo_square';
 function App() {
   return (
     <>
-      <div className='bg-TD-LightPink w-screen h-screen flex justify-start pt-6 pb-6 pl-14 pr-14'>
-        <Logotipo_square></Logotipo_square>
+      <div className='bg-TD-LightPink w-screen min-h-screen m-0 pt-6 pb-6 pl-4 pr-6'>
+      <Logotipo_square></Logotipo_square>
       </div>
     </>
   )
--- src/components/Logotipo_square.jsx
+++ src/components/Logotipo_square.jsx
@@ -3,8 +3,17 @@ import '../../src/App.css';
 function Logotipo_square() {
     return (
         <>
-        <div className='bg-white w-[500px] h-[250px] p-9 mt-[20px] rounded-[80px]'>
-           <div className='grid justify-self-start'><p className='font_logotipo'>The Tote</p></div>
+        <div className='bg-white w-full h-[200px] rounded-[80px] sm:w-[500px] sm:h-[250px] sm:p-9  sm:rounded-[80px]'>
+           <div className='grid justify-self-center'>
+            <p 
+              className='font_logotipo'>
+                The tote
+            </p>
+            <p 
+              className='font_dream'>
+                DREAM
+            </p>
+        </div>
         </div>
 
     </>
