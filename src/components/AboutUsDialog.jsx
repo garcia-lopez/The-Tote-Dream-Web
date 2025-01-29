abdf7887ebd009264bd1729220b63c4e65c7fb64
garcia-lopez <mplg070605@gmail.com>
2025-01-28 20:31:12 -0600

    Feat: added about us dialog

--- src/components/AboutUsDialog.jsx
+++ src/components/AboutUsDialog.jsx
@@ -1,5 +1,6 @@
 import { motion } from "motion/react";
 import Backdrop from './Backdrop';
+import '../../src/App.css';
 import close_icon from '../assets/close_icon.png'
 import BeigeTote from '../assets/BeigeTote.png'
 
@@ -26,23 +27,33 @@ const dropIn = { //Possible animation states
 
 const AboutUsDialog = ({handleClose}) => {
     return (
-        <Backdrop onClick={handleClose}>
-          <motion.div
-            onClick={(e) => e.stopPropagation()}
-            className='p-4 bg-[#EAD9D1] rounded-[10px] drop-shadow-2xl'
-            variants={dropIn}
-            initial="hidden"
-            animate="visible"
-            exit="exit"
-          >
-           <div className="m-auto grid w-full" onClick={handleClose}>
-            <div className='place-self-end w-[50px] hover:cursor-pointer'>
-              <img src={close_icon}></img>
-            </div>
-        </div>
-
-          </motion.div>
-        </Backdrop>
+<Backdrop onClick={handleClose}>
+  <motion.div
+    onClick={(e) => e.stopPropagation()}
+    className='relative p-4 bg-[#EAD9D1] rounded-[10px] drop-shadow-2xl'
+    variants={dropIn}
+    initial="hidden"
+    animate="visible"
+    exit="exit"
+  >
+    <div 
+       className="m-auto grid w-[300px] h-[450px] lg:w-[500px] lg:h-[500px]" 
+       onClick={handleClose}>
+      <div className=' w-[30px] lg:w-[50px] hover:cursor-pointer justify-self-end'>
+        <img src={close_icon}></img>
+      </div>
+      <img src={BeigeTote} className="absolute h-[250px] lg:h-[300px] origin-bottom -rotate-12 left-16 z-0"></img>
+      <img src={BeigeTote} className="absolute h-[250px] lg:h-[300px] origin-top-left rotate-12 scale-60 bottom-6 right-0 z-0"></img>
+      <p className="absolute font_about_us top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-[20px] lg:text-[25px] z-5"
+       >The Tote Dream creates personalized tote bags that reflect your style and story.
+        <br /> With care and creativity, we deliver functional designs 
+        <br /> that make every 
+        <br /> bag unique and 
+        <br /> meaningful.
+      </p>
+    </div>
+  </motion.div>
+</Backdrop>
     )
 
 };
